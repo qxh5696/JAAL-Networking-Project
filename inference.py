c93afd48a730f5ed2b720ea76aafcc1990527eff
Qadir Haqq <haqq.q173@gmail.com>
2019-12-01 20:49:26 -0500

    Moved comments to header, need to hardcode Q vectors

--- inference.py
+++ inference.py
@@ -3,14 +3,24 @@ File: inference.py
 Description: Inference module, functionality to be implemented in future work.
 Language: python3
 Authors: Qadir Haqq, Theodora Bendlin, John Tran
-"""
 
-import pandas as pd
+Example SNORT Rule:
+alert[0] tcp[1] $EXTERNAL_NET[2] any[3] -> $HOME_NET[4] 22[5] (msg: "INDICATORSCAN
+SSH brute force login attempt"[6]; flow: to_server[7], established[8]; content:
+"SSH-"[9]; depth: 4[10]; detection_filter: track by_src[11], count 5[12], seconds 60[13];
+metadata: service ssh[14]; classtype: misc-activity[15]; sid: 19559[16]; rev:5 [17];).
+
+The rule postulates that an alert must be generated if 5 packets
+destined for the home network were received within the last 60s,
+with port number 22.
+
+To translate this rule into a question vector, Jaal initializes a vector of size 18 with −1 set for every position
+
+Then, the position corresponding to the IP address is set to the normalized home network IP address and the position
+corresponding to port number is set to 22 (normalized version).
+"""
 import numpy as np
 import math
-
-from util import parse_pcap_packets
-from summarize import summarize_packet_data
 
 def create_aggregate_summary(summaries):
     agg_summaries = []
@@ -70,21 +80,6 @@ def postprocess_header_index(agg_sum, h_idx, t_v):
     
     return False
 
-# Example SNORT Rule:
-# alert[0] tcp[1] $EXTERNAL_NET[2] any[3] -> $HOME_NET[4] 22[5] (msg: "INDICATORSCAN
-# SSH brute force login attempt"[6]; flow: to_server[7], established[8]; content:
-# "SSH-"[9]; depth: 4[10]; detection_filter: track by_src[11], count 5[12], seconds 60[13];
-# metadata: service ssh[14]; classtype: misc-activity[15]; sid: 19559[16]; rev:5 [17];).
-#
-# The rule postulates that an alert must be generated if 5 packets
-# destined for the home network were received within the last 60s,
-# with port number 22.
-#
-# To translate this rule into a question vector, Jaal initializes a vector of size 18 with −1 set for every position
-#
-# Then, the position corresponding to the IP address is set to the normalized home network IP address and the position
-# corresponding to port number is set to 22 (normalized version).
-def translate(snort_rule):
-    vector = [-1] * 18 # initialization vector
+
     
 
